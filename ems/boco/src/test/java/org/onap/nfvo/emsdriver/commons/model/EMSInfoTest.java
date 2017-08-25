5a3d142ea88cb0e76735fec815a81f74c2fc295b
wangguirong <wangguirong@boco.com.cn>
2017-08-25 13:55:52 +0800

    add test case
    Change-Id: I7ee2ea17dc1ba818d32937ebcbbd106b06af21cc
    Signed-off-by: wangguirong <wangguirong@boco.com.cn>
    Issue-ID: VFC-162

---
+++ ems/boco/src/test/java/org/onap/nfvo/emsdriver/commons/model/EMSInfoTest.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright 2017 BOCO Corporation.  CMCC Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onap.nfvo.emsdriver.commons.model;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EMSInfoTest {
+	
+	private EMSInfo EMSInfo;
+
+    @Before
+    public void setUp() {
+    	EMSInfo = new EMSInfo();
+    }
+
+    @Test
+    public void testEMSInfo() {
+    	EMSInfo.setName("emsName");
+    	Map<String,CollectVo> collectMap = new HashMap<String,CollectVo>();
+    	CollectVo collectVo = new CollectVo();
+    	collectVo.setEmsName("emsName");
+    	collectMap.put("pm", collectVo);
+    	EMSInfo.putCollectMap("pm", collectVo);
+    	
+        assertNotNull(EMSInfo.toString());
+    }
+}
---
+++ ems/boco/src/test/java/org/onap/nfvo/emsdriver/configmgr/ConfigurationManagerTest.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright 2017 BOCO Corporation.  CMCC Technologies Co., Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onap.nfvo.emsdriver.configmgr;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Properties;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.onap.nfvo.emsdriver.commons.model.EMSInfo;
+
+public class ConfigurationManagerTest {
+
+	private ConfigurationManager configurationManager;
+	@Before
+    public void setUp() throws IOException {
+		configurationManager = new ConfigurationManager();
+		configurationManager.dispose();
+    }
+	
+	@Test
+	public void getAllEMSInfos() {
+		
+		List<EMSInfo> list = ConfigurationManager.getAllEMSInfos();
+		
+		assertTrue(list.size() >0);
+	}
+	
+	@Test
+	public void getEMSInfoByName() {
+		
+		EMSInfo eMSInfo = ConfigurationManager.getEMSInfoByName("1234");
+		
+		assertNotNull(eMSInfo);
+	}
+	
+	@Test
+	public void getProperties() {
+		
+		Properties properties = ConfigurationManager.getProperties();
+		
+		assertNotNull(properties);
+	}
+	
+	
+}
