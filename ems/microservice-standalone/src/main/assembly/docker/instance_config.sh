93a07cc478b4c1f201777197e96782f42be0e8e3
fujinhua <fu.jinhua@zte.com.cn>
2017-09-13 13:31:30 +0800

    Fix ems-driver docker script
    Fix msbAddress= to msbAddress:
    
    Change-Id: Ie3d821043944cdf6216b321061b59ec302f2a62d
    Issue-Id: VFC-351
    Signed-off-by: fujinhua <fu.jinhua@zte.com.cn>

--- ems/microservice-standalone/src/main/assembly/docker/instance_config.sh
+++ ems/microservice-standalone/src/main/assembly/docker/instance_config.sh
@@ -3,7 +3,7 @@
 MSB_IP=`echo $MSB_ADDR | cut -d: -f 1`
 MSB_PORT=`echo $MSB_ADDR | cut -d: -f 2`
 
-sed -i "s|msbAddress.*|msbAddress = $MSB_IP:$MSB_PORT|" emsdriver/conf/emsdriver.yml
+sed -i "s|msbAddress.*|msbAddress: $MSB_IP:$MSB_PORT|" emsdriver/conf/emsdriver.yml
 sed -i "s|\"ip\": \".*\"|\"ip\": \"$SERVICE_IP\"|" emsdriver/conf/emsdriver.yml
 
 
