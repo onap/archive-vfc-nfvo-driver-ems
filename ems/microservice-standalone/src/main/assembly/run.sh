b1800959422f1cc52f0fdea127d3c2f3856e31db
yangyan <yangyanyj@chinamobile.com>
2017-10-17 09:14:18 +0800

    Update ems Dockerfile
    Add +x for run.sh
    
    Issue-ID: VFC-345
    Change-Id: Id3a79d1228629d113c39377f0efedcb439b30a92
    Signed-off-by: yangyan <yangyanyj@chinamobile.com>

--- ems/microservice-standalone/src/main/assembly/run.sh
+++ ems/microservice-standalone/src/main/assembly/run.sh
@@ -90,5 +90,5 @@ echo @JAVA_OPTS@ $JAVA_OPTS
 
 class_path="$RUNHOME/:$RUNHOME/boco-driver.jar"
 echo @class_path@ $class_path
-
+# Start
 "$JAVA" $JAVA_OPTS -classpath "$class_path"  org.onap.vfc.nfvo.emsdriver.EMSDriverApp server "$RUNHOME/conf/emsdriver.yml"
