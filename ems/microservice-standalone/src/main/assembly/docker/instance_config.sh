1f44e6d1daef2f32211e7cfb9056db9464f13470
wangguirong <wangguirong@boco.com.cn>
2017-10-23 10:55:21 +0800

    Modify VES configuration
    Change-Id: I10f87585724785ce8dd80a0aba38c09c5e7f54d6
    Issue-Id: VFC-548
    Signed-off-by: wangguirong <wangguirong@boco.com.cn>

--- ems/microservice-standalone/src/main/assembly/docker/instance_config.sh
+++ ems/microservice-standalone/src/main/assembly/docker/instance_config.sh
@@ -5,7 +5,13 @@ MSB_PORT=`echo $MSB_ADDR | cut -d: -f 2`
 
 sed -i "s|msbAddress.*|msbAddress: $MSB_IP:$MSB_PORT|" emsdriver/conf/emsdriver.yml
 sed -i "s|\"ip\": \".*\"|\"ip\": \"$SERVICE_IP\"|" emsdriver/conf/emsdriver.yml
+sed -i "s|msbAddress=.*|msbAddress=$MSB_IP:$MSB_PORT|" emsdriver/conf/config.properties
+sed -i "s|event_api_url=.*|event_api_url=$EVENT_API_URL|" emsdriver/conf/config.properties
+sed -i "s|port=.*|port=$PORT|" emsdriver/conf/config.properties
+sed -i "s|username=.*|username=$USER_NAME|" emsdriver/conf/config.properties
+sed -i "s|password=.*|password=$PASSWORD|" emsdriver/conf/config.properties
 
 
 cat emsdriver/conf/emsdriver.yml
+cat emsdriver/conf/config.properties
 
