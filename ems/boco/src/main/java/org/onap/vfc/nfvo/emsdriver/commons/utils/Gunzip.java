2dc4593a00f2319900545dbb651d3c27fd531194
Santosh Yadav <santosh.k.yadav@ril.com>
2018-01-31 12:35:37 +0530

    check actual no of bytes skipped
    checked no of byted skipped in skipped var
    line no: 65
    
    Issue-ID: VFC-697
    Change-Id: Idcc0d461d7b758532facac6014245abbb1da4d2b
    Signed-off-by: Santosh Yadav <santosh.k.yadav@ril.com>

--- ems/boco/src/main/java/org/onap/vfc/nfvo/emsdriver/commons/utils/Gunzip.java
+++ ems/boco/src/main/java/org/onap/vfc/nfvo/emsdriver/commons/utils/Gunzip.java
@@ -23,14 +23,12 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.zip.GZIPInputStream;
 
-
 public class Gunzip {
-	
+
 	/**
 	 * 
 	 */
-	public void unCompress(String gzFileName, String toFile) 
-		throws IOException {
+	public void unCompress(String gzFileName, String toFile) throws IOException {
 		GZIPInputStream gzIn = null;
 		FileOutputStream fileOutput = null;
 		FileInputStream gzInput = new FileInputStream(gzFileName);
@@ -41,19 +39,18 @@ public class Gunzip {
 			fileOutput = new FileOutputStream(tofile, false);
 
 			moveBytes(gzIn, fileOutput, -1, -1, 1024);
-		} finally{
-			if(gzIn != null){
+		} finally {
+			if (gzIn != null) {
 				gzIn.close();
 			}
-			if(fileOutput != null){
+			if (fileOutput != null) {
 				fileOutput.close();
 			}
-			
+
 		}
-		
-		
+
 	}
-	
+
 	private void enable(File tofile) throws IOException {
 		if (!tofile.exists()) {
 			String parentPath = tofile.getParent();
@@ -63,32 +60,30 @@ public class Gunzip {
 		}
 	}
 
-
-	public long moveBytes(InputStream input, OutputStream output, long off, long len, int bufsize) 
-			throws IOException {
-			if (off > 0)
-				input.skip(off);
-			
-			long totalNum = 0;
-			byte[] buf = new byte[bufsize];
-
-			while (true) {
-				if (len>0 && (len-totalNum)<=0)
-					break;
-				
-				else if (len>0 && bufsize>(len-totalNum))
-					bufsize = (int)(len-totalNum);
-				
-				int readNum = input.read(buf, 0, bufsize);
-				if (readNum <= 0)
-					break;
-				
-				output.write(buf, 0, readNum);
-				totalNum += readNum;
-			}
-			buf = null;
-			return totalNum;
+	public long moveBytes(InputStream input, OutputStream output, long off, long len, int bufsize) throws IOException {
+		if (off > 0) {
+			long skipped = input.skip(off);
+           // need to check actual no of bytes skipped and decide the flow
 		}
-	
-	
+		long totalNum = 0;
+		byte[] buf = new byte[bufsize];
+
+		while (true) {
+			if (len > 0 && (len - totalNum) <= 0)
+				break;
+
+			else if (len > 0 && bufsize > (len - totalNum))
+				bufsize = (int) (len - totalNum);
+
+			int readNum = input.read(buf, 0, bufsize);
+			if (readNum <= 0)
+				break;
+
+			output.write(buf, 0, readNum);
+			totalNum += readNum;
+		}
+		buf = null;
+		return totalNum;
+	}
+
 }
