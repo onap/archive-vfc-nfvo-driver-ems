5c1074378a97b719a36978241b5817aeb354d578
yangyan <yangyanyj@chinamobile.com>
2020-02-18 09:05:51 +0800

    Fix problems accessing the MSB interface
    Change-Id: Ie4deed6fbef015fa8f6292519e4b804e6ec4c43d
    Issue-ID: VFC-1621
    Signed-off-by: yangyan <yangyanyj@chinamobile.com>

--- ems/microservice-standalone/src/main/assembly/docker/docker-entrypoint.sh
+++ ems/microservice-standalone/src/main/assembly/docker/docker-entrypoint.sh
@@ -14,8 +14,12 @@ echo "MSB_ADDR=$MSB_ADDR"
 
 # Wait for MSB initialization
 echo "Wait for MSB initialization"
-for i in {1..20}; do
-    curl -sS -m 1 $MSB_ADDR > /dev/null && break
+for i in {1..5}; do
+    curl -sS -m 1 $MSB_PROTO:$MSB_ADDR/msb -k > /dev/null
+    res=$i?
+    if [ $res -ne 0 ]; then
+        break
+    fi
     sleep $i
 done
 
