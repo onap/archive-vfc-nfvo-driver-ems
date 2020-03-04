bb7fe6b7d2c3de9e85a14e843317884a8f585da3
yangyan <yangyanyj@chinamobile.com>
2020-03-04 16:35:40 +0800

    Fix type error
    Change-Id: Id7d14a1630955c5e2a2067aaee4ffa4649470f6e
    Issue-ID: VFC-1626
    Signed-off-by: yangyan <yangyanyj@chinamobile.com>

--- ems/microservice-standalone/src/main/assembly/docker/docker-env-config.sh
+++ ems/microservice-standalone/src/main/assembly/docker/docker-env-config.sh
@@ -2,37 +2,36 @@
 
 install_sf(){
 
-        sed -i 's/enabled=1/enabled=0/' /etc/yum/pluginconf.d/fastestmirror.conf
-        sed -i 's|#baseurl=http://mirror.centos.org/centos|baseurl=http://mirrors.ocf.berkeley.edu/centos|' /etc/yum.repos.d/*.repo
-        yum -y update
-        yum -y install wget unzip socat java-1.8.0-openjdk-headless
-        sed -i 's|#networkaddress.cache.ttl=-1|networkaddress.cache.ttl=10|' /usr/lib/jvm/jre/lib/security/java.security
+	sed -i 's/enabled=1/enabled=0/' /etc/yum/pluginconf.d/fastestmirror.conf
+	sed -i 's|#baseurl=http://mirror.centos.org/centos|baseurl=http://mirrors.ocf.berkeley.edu/centos|' /etc/yum.repos.d/*.repo
+	yum -y update
+	yum -y install wget unzip socat java-1.8.0-openjdk-headless
+	sed -i 's|#networkaddress.cache.ttl=-1|networkaddress.cache.ttl=10|' /usr/lib/jvm/jre/lib/security/java.security
 
-        mkdir emsdriver
+	mkdir emsdriver
 
-        # get binary zip from nexus
-        wget -q -O emsdiver-standalone.zip 'https://nexus.onap.org/service/local/artifact/maven/redirect?r=snapshots&g=org.onap.vfc.nfvo.driver.ems.ems&a=emsdriv
-er-standalone&v=LATEST&e=zip' && \
-            unzip emsdiver-standalone.zip -d emsdriver && \
-            rm -rf emsdiver-standalone.zip
+	# get binary zip from nexus
+	wget -q -O emsdiver-standalone.zip 'https://nexus.onap.org/service/local/artifact/maven/redirect?r=snapshots&g=org.onap.vfc.nfvo.driver.ems.ems&a=emsdriver-standalone&v=LATEST&e=zip' && \
+	    unzip emsdiver-standalone.zip -d emsdriver && \
+	    rm -rf emsdiver-standalone.zip
 
-        chmod +x /service/emsdriver/*.sh
-        chmod +x /service/emsdriver/docker/*.sh
+	chmod +x /service/emsdriver/*.sh
+	chmod +x /service/emsdriver/docker/*.sh
 }
 
 add_user(){
 
-        useradd onap
-        yum -y install sudo
-        chmod u+x /etc/sudoers
-        sed -i '/Same thing without a password/a\onap    ALL=(ALL:ALL) NOPASSWD:ALL' /etc/sudoers
-        chmod u-x /etc/sudoers
-        chown onap:onap -R /service
+	useradd onap
+	yum -y install sudo
+	chmod u+x /etc/sudoers
+	sed -i '/Same thing without a password/a\onap    ALL=(ALL:ALL) NOPASSWD:ALL' /etc/sudoers
+	chmod u-x /etc/sudoers
+	chown onap:onap -R /service
 }
 
 clean_sf_cache(){
-
-        yum clean all
+	
+	yum clean all
 }
 
 install_sf
